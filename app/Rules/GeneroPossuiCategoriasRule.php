fc7d99747008f26fb40fdad2d4d497bfc3d583dd
christopher <christopher.fernandes@before.com.br>
2020-08-03 23:09:40 -0400

    implementando validacao de generos com categorias

---
+++ app/Rules/GeneroPossuiCategoriasRule.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+class GeneroPossuiCategoriasRule implements Rule
+{
+    /** @var array */
+    private $categoriasId;
+
+    /** @var array */
+    private $generosId;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(array $categoriasId)
+    {
+        $this->categoriasId = array_unique($categoriasId);
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $this->generosId = array_unique($value);
+        if (!count($this->generosId) || !count($this->categoriasId)) {
+            return false;
+        }
+
+        $categoriasEncontradas = [];
+        foreach ($this->generosId as $generoId) {
+            $categoriasPorGenero = $this->getCategoriasPorGeneros($generoId);
+            if (!$categoriasPorGenero->count()) {
+                return false;
+            }
+            array_push($categoriasEncontradas, ...$categoriasPorGenero->pluck('categoria_id')->toArray());
+        }
+
+        if (count($categoriasEncontradas) !== count($this->categoriasId)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function getCategoriasPorGeneros($generoId): Collection
+    {
+        return DB::table('categoria_genero')
+            ->where('genero_id', $generoId)
+            ->whereIn('categoria_id', $this->categoriasId)
+            ->get();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'O genero deve possuir ao menos uma categoria';
+    }
+}
